7786e6aa9d36de2ca7cddee4b4ba80559aa34a10
Babila <babila.lima30@gmail.com>
2019-01-27 13:00:08 -0500

    remove import dash_table line

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table
 
 import pandas as pd
 import plotly.graph_objs as go
